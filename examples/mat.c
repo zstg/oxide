6fe60293843e1f1dcca8ee9475284becff7ef803
zstg <o0vckutt@duck.com>
2025-03-29 20:42:22 +0530

    Commit

--- examples/mat.c
+++ examples/mat.c
@@ -1,16 +1,18 @@
-// Quoted from oxide
-// > How to run:
-// >
-// >   $ ./oxide examples/mat.c > tmp-mat.s
-// >   $ gcc -static -o tmp-mat tmp-mat.s
-// >   $ ./tmp-mat
+int fibdp[100];
+int fib(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  } else if (fibdp[n] != 0) {
+    return fibdp[n];
+  } else {
+    fibdp[n] = fib(n-2) + fib(n-1);
+    return fibdp[n];
+  }
+}
 
 int main() {
-    int A[2][2] = {{1, 2}, {3, 4}};
-    int B[2][2] = {{5, 6}, {7, 8}};
-    int C[2][2];
-    
-    for (int i = 0; i < 2; i++)
-        for (int j = 0; j < 2; j++)
-            C[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j];
+  for (int i = 0; i < 100; i++) 
+    fibdp[i] = 0;
+  int ans = fib(46);
+  return ans;
 }
